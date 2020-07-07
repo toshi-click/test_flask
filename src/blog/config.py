e6265aa7051223ffdb52fa84a88c553c1291ff17
toshi <toshi@toshi.click>
2020-07-08 03:52:01 +0900

    sqlite使用の準備

--- src/blog/config.py
+++ src/blog/config.py
@@ -2,3 +2,7 @@ DEBUG = True
 SECRET_KEY="secret key"
 USERNAME = "hoge"
 PASSWORD = "hage"
+
+# DB(sqlite)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
