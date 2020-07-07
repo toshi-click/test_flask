e66b924eb9707f2c2d95b712ef30bf9d4e484d68
toshi <toshi@toshi.click>
2020-07-08 04:26:07 +0900

    DBの作成

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # test_flask
+
+# DB作成
+```bash
+docker exec -it flask bash
+python3 manage. py init_ db
+```
---
+++ src/manage.py
@@ -0,0 +1,9 @@
+from flask_script import Manager
+from blog import app
+
+from blog.scripts.db import InitDB
+
+if __name__== "__main__":
+    manager = Manager(app)
+    manager.add_command('init_db', InitDB)
+    manager.run()
